1381dfefc6df4ededee3ce4f11b2080dfa731927
MathisBach <mathisbachdu12@gmail.com>
2024-03-04 11:02:23 +0100

    1er

--- database-connection.php
+++ database-connection.php
@@ -8,10 +8,10 @@ pour que la variable $databaseConnection soit utilisable dans votre page
 global $databaseConnection;
 
 /* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
-$databaseConnection = mysqli_connect(
-    'URL de la machine hébergeant votre base de données',
-    "nom de l'utilisateur",
-    "mot de passe de l'utilisateur",
-    "nom de la base de données à laquelle se connecter",
-    "numéro de port ouvert"
+$databaseConnection  = mysqli_connect(
+    "localhost",
+    "root",
+    null,
+    "tp_pokemon2",
+    3306
 );
